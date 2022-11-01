02ed836b3ea814758ce2370ae4cbf85c434baa0e
Sunilb <Sunilbhave69@gmail.com>
2022-11-01 10:29:58 +0530

    library

---
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022, Sunil BHave and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+#from frappe.model.docstatus import DocStatus
+
+class LibraryMember(Document):
+	def before_save(self):
+		self.full_name = f'{self.fname} {self.last_name}'
+		
---
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2022, Sunil BHave and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+#from frappe.model.docstatus import DocStatus
+
+class LibraryMembership(Document):
+	def before_submit(self):
+		exists = frappe.db.exists(
+			'Library Membership',
+			{
+				'library_member' : self.library_member,
+				'docstatus' : 1,
+				'to_date' : ('>',self.from_date),
+
+			},
+		)
+		if exists:
+			frappe.throw('There is an Active Membership of this user')
+		
